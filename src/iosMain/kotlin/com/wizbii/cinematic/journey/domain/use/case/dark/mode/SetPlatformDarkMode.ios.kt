58817108d80afd520affc39d10a2bef47e4ae6d2
Gaël Ribes <ribesg@gmail.com>
2024-03-07 14:52:49 +0100

    Fixed status bar text color on iOS < 17

--- src/iosMain/kotlin/com/wizbii/cinematic/journey/domain/use/case/dark/mode/SetPlatformDarkMode.ios.kt
+++ src/iosMain/kotlin/com/wizbii/cinematic/journey/domain/use/case/dark/mode/SetPlatformDarkMode.ios.kt
@@ -1,5 +1,15 @@
 package com.wizbii.cinematic.journey.domain.use.case.dark.mode
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import platform.UIKit.UIApplication
+import platform.UIKit.UIStatusBarStyleDarkContent
+import platform.UIKit.UIStatusBarStyleLightContent
+import platform.UIKit.setStatusBarStyle
+
 actual suspend fun setPlatformDarkMode(isDarkMode: Boolean) {
-    // NOOP
+    withContext(Dispatchers.Main) {
+        val statusBarStyle = if (isDarkMode) UIStatusBarStyleLightContent else UIStatusBarStyleDarkContent
+        UIApplication.sharedApplication.setStatusBarStyle(statusBarStyle, true)
+    }
 }
