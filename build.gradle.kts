04a2928c1e0a54c63d6d702e7450531a954e8b0e
Gaël Ribes <ribesg@gmail.com>
2024-03-19 10:17:02 +0100

    Set version to 0.0.7-SNAPSHOT for development

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ val artifactId = "cinematic.journey"
 val groupId = "com.wizbii"
 val packageId = "$groupId.$artifactId"
 
-val appVersionName = "0.0.6"
+val appVersionName = "0.0.7-SNAPSHOT"
 val appVersionCode = appVersionName
     .removeSuffix("-SNAPSHOT")
     .split('.')
