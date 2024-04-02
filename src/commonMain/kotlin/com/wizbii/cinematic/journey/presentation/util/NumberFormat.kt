3545c797a19f3d3fdacbbb00fcb54b0412278eb6
Gaël Ribes <ribesg@gmail.com>
2024-04-02 11:48:44 +0200

    Rewrite NumberFormat, enhance tests

--- src/commonMain/kotlin/com/wizbii/cinematic/journey/presentation/util/NumberFormat.kt
+++ src/commonMain/kotlin/com/wizbii/cinematic/journey/presentation/util/NumberFormat.kt
@@ -2,16 +2,47 @@ package com.wizbii.cinematic.journey.presentation.util
 
 import kotlin.math.roundToLong
 
-fun Double.toString(maxDecimals: Int = 1): String {
-    require(maxDecimals >= 0)
-    if (maxDecimals == 0) return roundToLong().toString()
-    var multiplier = 1
-    repeat(maxDecimals) { multiplier *= 10 }
-    val rounded = (this * multiplier).roundToLong()
-    val integerPart = rounded / multiplier
-    val decimalPart = (rounded % multiplier).toDouble() / multiplier
-    return (integerPart + decimalPart).toString()
-}
+fun Number.toString(maxDecimals: Int = 1): String {
 
-fun Float.toString(maxDecimals: Int = 1): String =
-    toDouble().toString(maxDecimals)
+    require(maxDecimals >= 0)
+
+    if (maxDecimals == 0) {
+        return toDouble().roundToLong().toString()
+    }
+
+    val rawAsString = toString()
+    val (rawInteger, rawDecimals) = rawAsString.split('.')
+
+    if (rawDecimals.length <= maxDecimals) {
+        return rawAsString + CharArray(maxDecimals - rawDecimals.length) { '0' }.concatToString()
+    }
+
+    var integerDigitsCount = rawInteger.length
+    var allDigits = (rawInteger + rawDecimals).map(Char::digitToInt).toIntArray()
+    var lastDecimalDigitIndex = rawInteger.length + maxDecimals - 1
+    val firstDiscardedDecimalDigit = allDigits[lastDecimalDigitIndex + 1]
+    if (firstDiscardedDecimalDigit >= 5) {
+        var currentIndex = lastDecimalDigitIndex
+        while (currentIndex >= 0 && allDigits[currentIndex] == 9) {
+            allDigits[currentIndex--] = 0
+        }
+        if (currentIndex == -1) {
+            allDigits = intArrayOf(1) + allDigits
+            integerDigitsCount++
+            lastDecimalDigitIndex++
+        } else {
+            allDigits[currentIndex]++
+        }
+    }
+
+    return buildString {
+        allDigits
+            .take(integerDigitsCount)
+            .forEach(::append)
+        append('.')
+        allDigits
+            .slice(integerDigitsCount..lastDecimalDigitIndex)
+            .forEach(::append)
+    }
+
+}
--- src/commonTest/kotlin/com/wizbii/cinematic/journey/presentation/util/NumberFormatTest.kt
+++ src/commonTest/kotlin/com/wizbii/cinematic/journey/presentation/util/NumberFormatTest.kt
@@ -62,4 +62,31 @@ class NumberFormatTest {
         assertEquals("0.67", (2 / 3f).toString(2))
     }
 
+    @Test
+    fun `Double formatting should display enough decimals even when maxDecimals is larger than the number of decimals`() {
+        assertEquals("1.2000000000", 1.2.toString(10))
+        assertEquals("1.2300000000", 1.23.toString(10))
+        assertEquals("1.2340000000", 1.234.toString(10))
+    }
+
+    @Test
+    fun `Float formatting should display enough decimals even when maxDecimals is larger than the number of decimals`() {
+        assertEquals("1.2000000000", 1.2f.toString(10))
+        assertEquals("1.2300000000", 1.23f.toString(10))
+        assertEquals("1.2340000000", 1.234f.toString(10))
+    }
+
+    @Test
+    fun `Rounding limit test`() {
+        assertEquals("2", 1.500001.toString(0))
+        assertEquals("1.0", 0.9995.toString(1))
+        assertEquals("1.0", 0.9999.toString(1))
+        assertEquals("1.0", 1.0499.toString(1))
+        assertEquals("1.1", 1.0500.toString(1))
+        assertEquals("1.000", 0.9995.toString(3))
+        assertEquals("1.000", 0.9999.toString(3))
+        assertEquals("1.050", 1.0499.toString(3))
+        assertEquals("1.050", 1.0500.toString(3))
+    }
+
 }
