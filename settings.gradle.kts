24c8744ed48c3fb500095fbcd41b40dc73159e55
Gaël Ribes <ribesg@gmail.com>
2024-05-29 13:50:42 +0200

    Enable Gradle build scan

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,5 +15,13 @@ pluginManagement {
 }
 
 plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
+    id("com.gradle.develocity") version "3.17.4"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+
+develocity {
+    buildScan {
+        termsOfUseUrl.set("https://gradle.com/help/legal-terms-of-use")
+        termsOfUseAgree.set("yes")
+    }
 }
